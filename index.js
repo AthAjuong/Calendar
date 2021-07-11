bcf92a4fed504a4ac13ce1901e88a0d2ca02a03c
AthAjuong <a.ajuong@gmail.com>
2021-07-11 12:29:53 +0000

    debugging issue: variables not defined

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 var currentDateAndTime = new Date();
 
-function getTodaysDate(currentDateAndTime) {
-    let today = currentDateAndTime.getDate();
-    let thisMonth = currentDateAndTime.getMonth();
-    let thisYear = currentDateAndTime.getFullYear();
-
-    var varDate = [today+'/'+thisMonth+'/'+thisYear];
-    return varDate;
-}
+let today = currentDateAndTime.getDate();
+let thisMonth = currentDateAndTime.getMonth();
+let thisYear = currentDateAndTime.getFullYear();
+var varDate = [today+'/'+thisMonth+'/'+thisYear];
 
 let todaysDate = currentDateAndTime.getTodaysDate();
 
