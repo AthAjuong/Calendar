3a4661de02377705baca013d51cc0b2dea27e29a
AthAjuong <a.ajuong@gmail.com>
2021-07-11 12:07:20 +0000

    added a function that would get today's date and set it to the p element in the #todays-date box

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
-var theTime = new Date();
+var currentDateAndTime = new Date();
 console.log(theTime);
 
-function getTodaysDate() {
-    
+function getTodaysDate(currentDateAndTime) {
+    let today = currentDateAndTime.getDate();
+    let thisMonth = currentDateAndTime.getMonth();
+    let thisYear = currentDateAndTime.getFullYear();
+
+    var varDate = [today+'/'+thisMonth+'/'+thisYear];
+    return varDate;
 }
+
+let todaysDate = currentDateAndTime.getTodaysDate();
+
+document.getElementById('todays-date').children[1].innerHTML = todaysDate;
