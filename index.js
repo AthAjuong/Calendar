57204b576b0292fdcc21bd4eb430169a54238d1a
AthAjuong <a.ajuong@gmail.com>
2021-07-13 17:15:21 +0000

    changed months back to an array

--- index.js
+++ index.js
@@ -6,20 +6,20 @@ let thisYear = currentDateAndTime.getFullYear();
 
 var varDate = [today+'/'+(thisMonth+1)+'/'+thisYear];
 
-let months = {
-    "January":      thisMonth[0],
-    "February":     thisMonth[1],
-    "March":        thisMonth[2],
-    "April":        thisMonth[3],
-    "May":          thisMonth[4],
-    "June":         thisMonth[5],
-    "July":         thisMonth[6],
-    "August":       thisMonth[7],
-    "September":    thisMonth[8],
-    "October":      thisMonth[9],
-    "November":     thisMonth[10],
-    "December":     thisMonth[11],
-};
+let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
 
 document.getElementById('todays-date').children[0].innerHTML = months[thisMonth];
 document.getElementById('todays-date').children[1].innerHTML = varDate;
