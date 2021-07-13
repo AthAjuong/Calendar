5322092849b468fd551fa383f52feb08607182e8
AthAjuong <a.ajuong@gmail.com>
2021-07-13 17:58:14 +0000

    added script file

---
+++ js/script.js
@@ -0,0 +1,33 @@
+var currentDateAndTime = new Date();
+
+let today = currentDateAndTime.getDate();
+let thisMonth = currentDateAndTime.getMonth();
+let thisYear = currentDateAndTime.getFullYear();
+var monthResult=0;
+
+if(thisMonth<9) {
+    monthResult = "0"+thisMonth;
+}
+else {
+    monthResult = thisMonth+1;
+}
+
+var varDate = `${today}'/'${monthResult}'/'${thisYear}`;
+
+let months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+];
+
+document.getElementById('todays-date').children[0].innerHTML = months[thisMonth];
+document.getElementById('todays-date').children[1].innerHTML = varDate;
